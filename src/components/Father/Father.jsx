7b21e604d7496f35382b8cab2d51e748ec9be939
Akas_Datta <012akasdatta@gmail.com>
2023-04-09 12:23:14 +0600

    some component added parrent to children

--- src/components/Father/Father.jsx
+++ src/components/Father/Father.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import Myself from '../Myself/Myself';
+import Brother from '../Brother/Brother';
+import Sister from '../Sister/Sister';
 
 const Father = () => {
     return (
         <div>
             <h3>Father</h3>
+            <section className='flex'>
+                <Myself></Myself>
+                <Brother></Brother>
+                <Sister></Sister>
+            </section>
         </div>
     );
 };
--- src/components/Myself/Myself.jsx
+++ src/components/Myself/Myself.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Special from '../Special/Special';
 
 const Myself = () => {
     return (
         <div>
             <h2>Myself</h2>
+            <Special></Special>
         </div>
     );
 };
